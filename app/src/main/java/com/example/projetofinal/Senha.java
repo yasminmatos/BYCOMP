97bf5b9329bfbe2436e157fa3efd2fbd30597c9e
aluno <fernanda.fregolon@gmail.com>
2022-10-10 17:21:10 -0300

    alterar senha finalizando

--- app/src/main/java/com/example/projetofinal/Senha.java
+++ app/src/main/java/com/example/projetofinal/Senha.java
@@ -3,8 +3,25 @@ package com.example.projetofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Senha extends AppCompatActivity {
+
+    TextView textView39;
+    TextView textView40;
+    TextView textView41;
+    EditText idSenhaAtual;
+    EditText idNovaSenha;
+    Button btAlterarSenha2;
+    Button btVoltarTela;
+
+     View p;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,3 +29,4 @@ public class Senha extends AppCompatActivity {
         setContentView(R.layout.activity_senha);
     }
 }
+
